876752ff7f935baf0065eed9027780dc2918a223
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2024-12-04 17:03:03 +0000

    前端传入grule,后端执行代码 2024-12-05 01:03:03

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+    "github.com/hyperjumptech/grule-rule-engine/ast"
+	"github.com/hyperjumptech/grule-rule-engine/builder"
+	"github.com/hyperjumptech/grule-rule-engine/engine"
+	"github.com/hyperjumptech/grule-rule-engine/pkg"
 )
+
+type ExponentData struct {
+	Check float64
+	Set   float64
+}
 
 func main() {
 	r := gin.Default()
@@ -15,8 +24,25 @@ func main() {
     })
     r.LoadHTMLGlob("templates/*")
 	r.GET("/index", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl", gin.H{
-            "title": "Main website",
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+    })
+    r.POST("/number/", func(c *gin.Context) {
+        textareaContent := c.PostForm("textarea")
+
+        exponent := &ExponentData{
+            Check: 6.67428e-11,
+            Set:   0,
+        }
+        dataContext := ast.NewDataContext()
+        dataContext.Add("ExponentData", exponent)
+        lib := ast.NewKnowledgeLibrary()
+        rb := builder.NewRuleBuilder(lib)
+        rb.BuildRuleFromResource("TestExponent", "1.0.0", pkg.NewBytesResource([]byte(textareaContent)))
+        eng1 := &engine.GruleEngine{MaxCycle: 5}
+        kb, _ := lib.NewKnowledgeBaseInstance("TestExponent", "1.0.0")
+        eng1.Execute(dataContext, kb)
+        c.JSON(http.StatusOK, gin.H{
+            "textareaContent": exponent.Set,
         })
     })
 	r.Run()
