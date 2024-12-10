5346e93f6ba95c0ec7e834480537e567160175a3
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2024-12-10 14:55:30 +0000

    2024-12-10 22:55:30

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+    "log"
 
 	"github.com/gin-gonic/gin"
     "github.com/hyperjumptech/grule-rule-engine/ast"
@@ -25,6 +26,13 @@ func main() {
     r.LoadHTMLGlob("templates/*")
 	r.GET("/index", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+    })
+    r.GET("/grule/form/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "grule.tmpl", gin.H{})
+    })
+    r.POST("/grule/form/", func(c *gin.Context) {
+        gruleForm := c.PostForm("grule_add")
+        log.Println(gruleForm)
     })
     r.POST("/number/", func(c *gin.Context) {
         textareaContent := c.PostForm("textarea")
