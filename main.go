dcdace97e9850e09800e8645d4440050ad697d75
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2024-12-14 08:39:08 +0000

    2024-12-14 16:39:07

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	routers.RegisterEbsRoutes(r)
-	routers.RegisterGruleRoutes(r)
-	routers.RegisterPriceRoutes(r)
-	r.GET("/", func(c *gin.Context) {
+	r := gin.Default() // 路由
+	r.LoadHTMLGlob("templates/*") // 模板
+	routers.RegisterEbsRoutes(r) // 云硬盘
+	routers.RegisterGruleRoutes(r) // 计费规则
+	routers.RegisterPriceRoutes(r) // 价格调整
+	r.GET("/", func(c *gin.Context) { // 首页
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
-	r.Run()
+	r.Run() // 运行在8080端口
 }
