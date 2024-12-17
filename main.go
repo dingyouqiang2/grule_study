04833ff9e160ba97c160b9fdcdc56528ea493dd9
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2024-12-17 06:07:18 +0000

    2024-12-17 14:07:17

--- main.go
+++ main.go
@@ -10,12 +10,9 @@ import (
 func main() {
 	r := gin.Default()            // 路由
 	r.LoadHTMLGlob("templates/*") // 模板
-	r.Static("/static", "./static")
+	r.Static("/static", "./static") // 静态文件
 	routers.RegisterApiRoutes(r)      // api路由
 	routers.RegisterConfigRoutes(r)   // 配置中心路由
-	routers.RegisterEbsRoutes(r)      // 云硬盘路由
-	routers.RegisterGruleRoutes(r)    // 计费规则路由
-	routers.RegisterPriceRoutes(r)    // 价格调整路由
 	r.GET("/", func(c *gin.Context) { // 首页路由
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
