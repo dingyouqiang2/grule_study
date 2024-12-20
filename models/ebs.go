05fde0f35b13ed7ee50bd0e4160b4ee237c028dd
dingyouqiang2 <dingyouqiang92@outlook.com>
2024-12-20 08:22:12 +0000

    2024-12-20 16:22:09

--- models/ebs.go
+++ models/ebs.go
@@ -2,13 +2,14 @@ package models
 
 // 云硬盘费用
 type EBS struct {
-	BillMode    int       // 按量(0)/包年月(1)
-	Region      string       // 区域
-	SyshdType   string    // 系统盘类型
-	InstanceCnt int      // 系统盘容量
-	UnitPrice   float32    // 单价
-	CycleCount  int       // 包月时长
-	Count       int           // 数量
+	BillMode    int     // 按量(0)/包年月(1)
+	Region      string  // 区域
+	SyshdType   string  // 系统盘类型
+	InstanceCnt int     // 系统盘容量
+	UnitPrice   float32 // 单价
+	CycleCount  int     // 包月时长
+	Count       int     // 数量
+	TotalCost   float32 //总费用
 }
 
 func (e *EBS) GetFieldDescMap() map[string]string {
